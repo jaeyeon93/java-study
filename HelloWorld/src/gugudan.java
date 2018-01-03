77b18b317143ce92289943b8e77a92d9b64ba42b
jimmy <foodsksms@gmail.com>
2018-01-03 17:39:57 +0900

    gugudan2 using method

--- HelloWorld/src/gugudan.java
+++ HelloWorld/src/gugudan.java
@@ -5,9 +5,11 @@ import java.util.Scanner;
 public class gugudan {
 	public static void main (String args []) {
 		System.out.println("구구단 중 출력할 단은 ?");
+		/*
 		Scanner scanner = new Scanner(System.in);
-		// make scanner object
 		int number = scanner.nextInt();
+		System.out.println("사용자가 입력한 값 : "+number);
+		
 		if (number < 2) {
 			System.out.println("2이상, 9이의 값만 입력할 수 있습니다.");
 		} else if (number > 9) {
@@ -20,5 +22,14 @@ public class gugudan {
 
 				}
 			}
+		*/
+		int [] result = new int[9];
+		for (int i = 0; i < result.length; i++) {
+			result[i] = 2 * (i+1);
+		}
+		
+		for (int i = 0; i < result.length; i++) {
+			System.out.println(result[i]);
+			}	
 		}		
 }
