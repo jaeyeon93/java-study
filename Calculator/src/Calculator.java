0e397b9d2aeb95de7fb481a8e172b68c332d9718
jimmy <foodsksms@gmail.com>
2018-01-04 14:45:29 +0900

    Make calculator step 3

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -3,28 +3,39 @@ import java.util.Scanner;
 public class Calculator {
 	public static void main(String [] args) {
 		Scanner scanner = new Scanner(System.in);
-		
-		System.out.println("첫 번째 입력 값 :  ");
+
+		System.out.print("초기값 :  ");
 		int first = scanner.nextInt();
 		System.out.println(first);
-		
-		System.out.println("두 번째 입력 값 : ");
-		int second = scanner.nextInt();
-		System.out.println(second);
-		System.out.println("기호 입력 값 : ");
-		String symbol = scanner.next();
-		// alt + command + 아래화살표 하면 바로밑에 복사됨.
-	
-		if ("+".equals(symbol)) {
-			System.out.println("덧셈 : " + (first + second));
-		} else if ("-".equals(symbol)) {
-			System.out.println("뺄셈 : " + (first - second));
-		} else if ("*".equals(symbol)) {
-			System.out.println("곱셈 : " + (first * second));
-		} else if ("/".equals(symbol)) {
-			System.out.println("나눗셈 : " + (first / second));
-		} else {
-			System.out.println("사칙연산에 해당하지 않는 값을 입력했습니다.");
+
+		int result = first;
+		while (true) {
+			System.out.print("기호 : ");
+			String symbol = scanner.next();
+			System.out.println(symbol);
+
+			if (symbol.equals("quit")) {
+				System.out.println("최종결과값 : "+ result);
+				break;
+			}
+
+			System.out.print("입력 값 :  ");
+			int input = scanner.nextInt();
+			System.out.println(input);
+
+
+
+			if ("+".equals(symbol)) {
+				System.out.println("덧셈결과 : " + (result+=input));
+			} else if ("-".equals(symbol)) {
+				System.out.println("뺄셈결과 : " + (result-=input));
+			} else if ("*".equals(symbol)) {
+				System.out.println("곱셈결과 : " + (result*=input));
+			} else if ("/".equals(symbol)) {
+				System.out.println("나눗셈결과 : " + (result/=input));
+			} else {
+				System.out.println("사칙연산에 해당하지 않는 값을 입력했습니다.");
+			}
 		}
 	}
 }
