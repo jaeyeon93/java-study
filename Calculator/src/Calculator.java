0ef0e5eef1061bfb68242a5783e43097f31e6a2e
jimmy <foodsksms@gmail.com>
2018-01-04 11:59:50 +0900

    java calculator project start

---
+++ Calculator/src/Calculator.java
@@ -0,0 +1,12 @@
+
+public class Calculator {
+	public static void main(String [] args) {
+		System.out.println("덧셈 : " + 3+4);
+		System.out.println("덧셈 : " + (3 + 4));
+		// alt + command + 아래화살표 하면 바로밑에 복사됨.
+		System.out.println("뺄셈 : " + (4 - 3));
+		System.out.println("곱셈 : " + (3 * 4));
+		System.out.println("나눗셈 : " + (4 / 2));
+		
+	}
+}
