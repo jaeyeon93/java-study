a1c65d3613f88be977a63f6fcb1457134fde7d4b
jimmy <foodsksms@gmail.com>
2018-01-04 12:10:33 +0900

    Calculator first step finish

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -1,12 +1,19 @@
+import java.util.Scanner;
 
 public class Calculator {
 	public static void main(String [] args) {
-		System.out.println("덧셈 : " + 3+4);
-		System.out.println("덧셈 : " + (3 + 4));
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("첫 번째 입력 값 :  ");
+		int first = scanner.nextInt();
+		System.out.println(first);
 		// alt + command + 아래화살표 하면 바로밑에 복사됨.
-		System.out.println("뺄셈 : " + (4 - 3));
-		System.out.println("곱셈 : " + (3 * 4));
-		System.out.println("나눗셈 : " + (4 / 2));
 		
+		System.out.println("두 번째 입력 값 : ");
+		int second = scanner.nextInt();
+		System.out.println(second);
+		System.out.println("덧셈 : " + (first + second));
+		System.out.println("뺄셈 : " + (first - second));
+		System.out.println("곱셈 : " + (first * second));
+		System.out.println("나눗셈 : " + (first / second));
 	}
 }
